fe174126fa230aca7c03efc34ce14a0c77db24ff
Khoa <ngyntienkhoa22999@gmail.com>
2023-10-16 22:03:55 +0700

    adjust layout for deestination

--- src/app/destination/[[...planet]]/layout.tsx
+++ src/app/destination/[[...planet]]/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PlanetImage from "../components/PlanetImage";
-import Tabs from "@/app/destination/components/Tabs";
+import Tabs from "../components/Tabs";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
